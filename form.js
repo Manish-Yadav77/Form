bbf4a55834b6df4815c2c9119924c975be7d630c
Manish-Yadav77 <manishkyadav969@gmail.com>
2024-06-12 23:23:09 +0530

    Second commit

--- form.js
+++ form.js
@@ -3,9 +3,9 @@ function reset()
     let name=document.getElementById("name")
     let email=document.getElementById("email")
     let msg=document.getElementById("msg")
-    if(name.value==""||email.value==""||msg.value=="")
+    if(name.value=="" && email.value=="" && msg.value=="")
         {
-            alert("please enter all details")
+            alert("please enter some details")
         }
         else{
             name.value=""
@@ -23,18 +23,34 @@ function submit()
         email:email.value,
         msg:msg.value,
     }    
-    if(name.value==""||email.value==""||msg.value=="")
+    if(name.value=="" || email.value=="" || msg.value=="")
         {
             alert("please enter all details")
         }
         else{
-            // localStorage.setItem("Details : ", obj.name+" " +obj.email+" "+obj.msg)
-            // console.log(obj)
-            localStorage.setItem("name : ",name.value)
-            localStorage.setItem("email : ",email.value)
-            localStorage.setItem("msg : ",msg.value)
+            localStorage.setItem("Details : ", obj.name+" " +obj.email+" "+obj.msg)
+            console.log(obj)
             name.value=""
             email.value=""
             msg.value="" 
         }
 }
+document.getElementById('name').addEventListener('keypress',function onfocus(event){
+    if(event.key=='Enter')
+        {
+            document.getElementById('email').focus();
+        }
+})
+document.getElementById('email').addEventListener('keypress',function onfocus(event){
+    if(event.key=='Enter')
+        {
+            document.getElementById('msg').focus();
+        }
+})
+document.getElementById('msg').addEventListener('keypress',function onfocus(event){
+    if(event.key=='Enter')
+        {
+            event.preventDefault();
+            submit();
+        }
+})
